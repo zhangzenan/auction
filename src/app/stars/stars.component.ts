d25d26b64f31646f4bb9f72d0c26cb5ad8e4883d
zhangzenan <zhangzenan@yiguo.com>
2017-09-22 18:01:34 +0800

    组件间通讯

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,25 +1,46 @@
-import { Component,Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, OnChanges, SimpleChanges, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
-  
+export class StarsComponent implements OnInit, OnChanges {
+
+
   //外部输入数据注解
   @Input()
-  rating:number=0;
-  
-  stars:boolean [];
+  rating: number = 0;
+
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
+
+  stars: boolean[];
+
+  @Input()
+  readonly: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-    this.stars=[];
-    for(let i=1;i<5;i++){
-      this.stars.push(i>this.rating);  
-    }    
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating);
+    }
+  }
+
+
+  clickStar(index: number) {
+    if (!this.readonly) {
+      this.rating = index + 1;
+
+      this.ratingChange.emit(this.rating);
+    }
   }
 
 }
